62d81655ea1eef4a33cf93893385a7dfedeb2a10
Your Name <119275530@qq.com>
2023-06-05 10:07:27 +0800

    6.5

--- lodash/JH7566-lodash.js
+++ lodash/JH7566-lodash.js
@@ -28,4 +28,34 @@ var jh7566 = {
     }
     return ary
   },
+
+  /**
+   *
+   * @param {*} array  被连接的数组。
+   * @param {*} arr 连接的值
+   * @returns
+   */
+  concat: function (array, arr) {
+    var result = []
+    var length = array.length + arr.length
+    for (var i = 0; i < length; i++) {
+      if (array[i]) {
+        result.push([array[i]])
+      } else {
+        i = 0
+        result.push([arr[i]])
+      }
+    }
+    return result
+  },
+
+
+  fill: function (array, value, start, end) {
+    var result = []
+    for(var i = 0;i < array.length;i++){
+      
+    }
+  }
 }
+
+
